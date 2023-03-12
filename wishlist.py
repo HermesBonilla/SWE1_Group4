6873f352a837dff16be67d54ab45fee26ab917da
Hermes Bonilla <hboni008@fiu.edu>
2023-03-12 19:04:26 -0400

    add melanie's code and rating feature

--- wishlist.py
+++ wishlist.py
@@ -1,9 +1,11 @@
 import json
 
+
 class Wishlist:
     def __init__(self, name):
         self.name = name
         self.items = []
+
 
 class User:
     def __init__(self, user_id):
@@ -11,14 +13,14 @@ class User:
         self.wishlists = {}
         self.shopping_cart = []  # testing: create an empty shopping cart for the user
 
-
     def create_wishlist(self, name):
         if name in self.wishlists:
-            print(f"Wishlist with name '{name}' already exists for user {self.user_id}")
+            print(
+                f"Wishlist with name '{name}' already exists for user {self.user_id}")
         else:
             self.wishlists[name] = Wishlist(name)
-            print(f"Wishlist with name '{name}' created for user {self.user_id}")
-
+            print(
+                f"Wishlist with name '{name}' created for user {self.user_id}")
 
     def list_wishlist_items(self, wishlist_names):
         wishlist_items = {}
@@ -26,20 +28,24 @@ class User:
             if wishlist_name in self.wishlists:
                 wishlist_items[wishlist_name] = self.wishlists[wishlist_name].items
             else:
-                print(f"Wishlist with name '{wishlist_name}' does not exist for user {self.user_id}")
-        return json.dumps(wishlist_items,indent = 1 ,separators =(',', ': '))  # indent = 2 for vertical format, separators =(',', ': ') for horizontal format
-
+                print(
+                    f"Wishlist with name '{wishlist_name}' does not exist for user {self.user_id}")
+        # indent = 2 for vertical format, separators =(',', ': ') for horizontal format
+        return json.dumps(wishlist_items, indent=1, separators=(',', ': '))
 
     def remove_book_to_cart(self, wishlist_name, book_name):
         if wishlist_name in self.wishlists:
             if book_name in self.wishlists[wishlist_name].items:
                 self.wishlists[wishlist_name].items.remove(book_name)
                 self.shopping_cart.append(book_name)
-                print(f"Book '{book_name}' removed from wishlist '{wishlist_name}' and added to shopping cart")
+                print(
+                    f"Book '{book_name}' removed from wishlist '{wishlist_name}' and added to shopping cart")
             else:
-                print(f"Book '{book_name}' does not exist in wishlist '{wishlist_name}'")
+                print(
+                    f"Book '{book_name}' does not exist in wishlist '{wishlist_name}'")
         else:
-            print(f"Wishlist with name '{wishlist_name}' does not exist for user {self.user_id}")
+            print(
+                f"Wishlist with name '{wishlist_name}' does not exist for user {self.user_id}")
 
 
 # create a user with ID 1
@@ -58,7 +64,7 @@ book2 = "1984"
 user1.wishlists["Retro"].items.append(book2)
 
 #book3 = "The Lord of the Rings"
-book3 = { "name": "John", "age": 30, "city": "New York"}
+book3 = {"name": "John", "age": 30, "city": "New York"}
 user1.wishlists["Retro"].items.append(book3)
 
 # list the items in the "Retro" and "Short" wishlists of user1 in JSON format
